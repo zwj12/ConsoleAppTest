f6daee4cf7453130b141143fcc81073fcf64f4ec
Michael-WeiJin Zhu <michael-weijin.zhu@cn.abb.com>
2019-08-16 09:11:07 +0800

    Add project files.

---
+++ ConsoleAppTest/EncryptionHelper.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    public enum EncryptionKeyEnum
+    {
+        KeyA,
+        KeyB
+    }
+
+    public class EncryptionHelper
+    {
+        string encryptionKeyA = "abbrawac";
+        string encryptionKeyB = "abbwacfpd";
+        string md5Begin = "m";
+        string md5End = "h";
+        string encryptionKey = string.Empty;
+
+        static void Main2(string[] args)
+        {
+            EncryptionHelper eh = new EncryptionHelper();
+            string strori = "1600-515088";
+            string stren = eh.EncryptString(strori);
+            Console.WriteLine(stren);
+            Console.WriteLine(eh.DecryptString(stren));
+            if("asd"!="asd")
+            {
+                Console.WriteLine("asd");
+            }
+            Console.ReadKey();
+            
+        }
+
+        public EncryptionHelper()
+        {
+            this.InitKey(EncryptionKeyEnum.KeyA);
+        }
+
+        public EncryptionHelper(EncryptionKeyEnum key)
+        {
+            this.InitKey(key);
+        }
+
+        private void InitKey(EncryptionKeyEnum key)
+        {
+            switch (key)
+            {
+                case EncryptionKeyEnum.KeyA:
+                    encryptionKey = encryptionKeyA;
+                    break;
+                case EncryptionKeyEnum.KeyB:
+                    encryptionKey = encryptionKeyB;
+                    break;
+            }
+        }
+
+        public string EncryptString(string str)
+        {
+            return Encrypt(str, encryptionKey);
+        }
+
+        public string DecryptString(string str)
+        {
+            return Decrypt(str, encryptionKey);
+        }
+
+        public string GetMD5String(string str)
+        {
+            str = string.Concat(md5Begin, str, md5End);
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.Unicode.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string md5String = string.Empty;
+            foreach (var b in targetData)
+            {
+                md5String += b.ToString("x2");
+            }
+            return md5String;
+        }
+
+        private string Encrypt(string str, string sKey)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByteArray = Encoding.Default.GetBytes(str);
+            des.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+            des.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            StringBuilder ret = new StringBuilder();
+            foreach (byte b in ms.ToArray())
+            {
+                ret.Append(b.ToString("X2"));
+                //ret.AppendFormat("{0:X2}", b);  
+            }
+            ret.ToString();
+            return ret.ToString();
+        }
+
+        private string Decrypt(string pToDecrypt, string sKey)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByteArray = new byte[pToDecrypt.Length / 2];
+            for (int x = 0; x < pToDecrypt.Length / 2; x++)
+            {
+                int i = (Convert.ToInt32(pToDecrypt.Substring(x * 2, 2), 16));
+                inputByteArray[x] = (byte)i;
+            }
+            des.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+            des.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            StringBuilder ret = new StringBuilder();
+            return System.Text.Encoding.Default.GetString(ms.ToArray(), 0, Convert.ToInt32(ms.Length));
+        }
+    }
+
+    public class RegistFileHelper
+    {
+        public static string ComputerInfofile = "ComputerInfo.key";
+        public static string RegistInfofile = "RegistInfo.key";
+        public static void WriteRegistFile(string info)
+        {
+            WriteFile(info, RegistInfofile);
+        }
+        public static void WriteComputerInfoFile(string info)
+        {
+            WriteFile(info, ComputerInfofile);
+        }
+        public static string ReadRegistFile()
+        {
+            return ReadFile(RegistInfofile);
+        }
+        public static string ReadComputerInfoFile()
+        {
+            return ReadFile(ComputerInfofile);
+        }
+        public static bool ExistComputerInfofile()
+        {
+            return File.Exists(ComputerInfofile);
+        }
+        public static bool ExistRegistInfofile()
+        {
+            return File.Exists(RegistInfofile);
+        }
+        private static void WriteFile(string info, string fileName)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(fileName, false))
+                {
+                    sw.Write(info);
+                    sw.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        private static string ReadFile(string fileName)
+        {
+            string info = string.Empty;
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    info = sr.ReadToEnd();
+                    sr.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return info;
+        }
+    }
+}
---
+++ ConsoleAppTest/egm-sensor.cs
@@ -0,0 +1,200 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using abb.egm;
+
+//////////////////////////////////////////////////////////////////////////
+// Sample program using protobuf-csharp-port 
+// (http://code.google.com/p/protobuf-csharp-port/wiki/GettingStarted)
+//
+// 1) Download protobuf-csharp binaries from https://code.google.com/p/protobuf-csharp-port/
+// 2) Unpack the zip file
+// 3) Copy the egm.proto file to a sub catalogue where protobuf-csharp was un-zipped, e.g. ~\protobuf-csharp\tools\egm
+// 4) Generate an egm C# file from the egm.proto file by typing in a windows console: protogen .\egm\egm.proto --proto_path=.\egm
+// 5) Create a C# console application in Visual Studio
+// 6) Install Nuget, in Visual Studio, click Tools and then Extension Manager. Goto to Online, find the NuGet Package Manager extension and click Download.
+// 7) Install protobuf-csharp via NuGet, select in Visual Studio, Tools Nuget Package Manager and then Package Manager Console and type PM>Install-Package Google.ProtocolBuffers
+// 8) Add the generated file egm.cs to the Visual Studio project (add existing item)
+// 9) Copy the code below and then compile, link and run.
+//
+// Copyright (c) 2014, ABB
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with
+// or without modification, are permitted provided that 
+// the following conditions are met:
+//
+//    * Redistributions of source code must retain the 
+//      above copyright notice, this list of conditions 
+//      and the following disclaimer.
+//    * Redistributions in binary form must reproduce the 
+//      above copyright notice, this list of conditions 
+//      and the following disclaimer in the documentation 
+//      and/or other materials provided with the 
+//      distribution.
+//    * Neither the name of ABB nor the names of its 
+//      contributors may be used to endorse or promote 
+//      products derived from this software without 
+//      specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF 
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        // listen on this port for inbound messages
+        public static int _ipPortNumber = 6510;
+        static void Main(string[] args)
+        {
+
+
+
+            Sensor s = new Sensor();
+            s.Start();
+
+            Console.WriteLine("Press any key to Exit");
+            Console.ReadLine();
+        }
+    }
+
+    class Sensor
+    {
+        private Thread _sensorThread = null;
+        private UdpClient _udpServer = null;
+        private bool _exitThread = false;
+        private uint _seqNumber = 0;
+
+        public void SensorThread()
+        {
+            // create an udp client and listen on any address and the port _ipPortNumber
+            _udpServer = new UdpClient(Program._ipPortNumber);
+            var remoteEP = new IPEndPoint(IPAddress.Any, Program._ipPortNumber);
+
+            while (_exitThread == false)
+            {
+                // get the message from robot
+                var data = _udpServer.Receive(ref remoteEP);
+                if (data != null)
+                {
+                    // de-serialize inbound message from robot
+                    EgmRobot robot = EgmRobot.CreateBuilder().MergeFrom(data).Build();
+
+                    // display inbound message
+                    DisplayInboundMessage(robot);
+
+                    // create a new outbound sensor message
+                    EgmSensor.Builder sensor = EgmSensor.CreateBuilder();
+                    CreateSensorMessage(sensor);
+
+                    using (MemoryStream memoryStream = new MemoryStream())
+                    {
+                        EgmSensor sensorMessage = sensor.Build();
+                        sensorMessage.WriteTo(memoryStream);
+
+                        // send the udp message to the robot
+                        int bytesSent = _udpServer.Send(memoryStream.ToArray(),
+                                                       (int)memoryStream.Length, remoteEP);
+                        if (bytesSent < 0)
+                        {
+                            Console.WriteLine("Error send to robot");
+                        }
+                        else
+                        {
+                            Console.WriteLine("data send to robot");
+                        }
+                    }
+                }
+            }
+        }
+
+        // Display message from robot
+        void DisplayInboundMessage(EgmRobot robot)
+        {
+            if (robot.HasHeader && robot.Header.HasSeqno && robot.Header.HasTm)
+            {
+                Console.WriteLine("Seq={0} tm={1}",
+                    robot.Header.Seqno.ToString(), robot.Header.Tm.ToString());
+            }
+            else
+            {
+                Console.WriteLine("No header in robot message");
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////
+        // Create a sensor message to send to the robot
+        void CreateSensorMessage(EgmSensor.Builder sensor)
+        {
+            // create a header
+            EgmHeader.Builder hdr = new EgmHeader.Builder();
+            hdr.SetSeqno(_seqNumber++)
+                .SetTm((uint)DateTime.Now.Ticks)
+                .SetMtype(EgmHeader.Types.MessageType.MSGTYPE_CORRECTION);
+
+            sensor.SetHeader(hdr);
+
+            // create some sensor data
+            EgmPlanned.Builder planned = new EgmPlanned.Builder();
+            EgmPose.Builder pos = new EgmPose.Builder();
+            EgmQuaternion.Builder pq = new EgmQuaternion.Builder();
+            EgmCartesian.Builder pc = new EgmCartesian.Builder();
+
+            //pc.SetX(1200)
+            //    .SetY(11.1)
+            //    .SetZ(1000);
+
+            pc.SetX(10)
+    .SetY(0)
+    .SetZ(0);
+
+            pq.SetU0(0.32557)
+                .SetU1(0.0)
+                .SetU2(0.94552)
+                .SetU3(0.0);
+
+            pos.SetPos(pc)
+                .SetOrient(pq);
+
+            planned.SetCartesian(pos);  // bind pos object to planned
+            sensor.SetPlanned(planned); // bind planned to sensor object
+
+            Console.WriteLine("CreateSensorMessage");
+
+            return;
+        }
+
+        // Start a thread to listen on inbound messages
+        public void Start()
+        {
+            _sensorThread = new Thread(new ThreadStart(SensorThread));
+            _sensorThread.Start();
+        }
+
+        // Stop and exit thread
+        public void Stop()
+        {
+            _exitThread = true;
+            _sensorThread.Abort();
+        }
+    }
+}
+
+
