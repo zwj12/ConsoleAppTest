ba25b7be8b3600b35d7e16806b8bd413d7ab3a22
zwj12 <zwj_12@163.com>
2020-12-06 20:09:28 +0800

    add cert

---
+++ ConsoleAppTest/Log/Bar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace Com.Foo
+{
+    class Bar
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Bar));
+
+        public void DoIt()
+        {
+            log.Debug("Did it again!");
+        }
+    }
+}
