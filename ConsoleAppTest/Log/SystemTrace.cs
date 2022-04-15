e4927bedebf7782dfc3fa3ea78bbd3f0130408a5
Michael-WeiJin Zhu <michael-weijin.zhu@cn.abb.com>
2022-04-15 10:33:58 +0800

    add

---
+++ ConsoleAppTest/Log/SystemTrace.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest.Log
+{
+    class SystemTrace
+    {
+        private static TextWriterTraceListener textWriterTraceListener;
+
+        public void WriteMichaelLog(string strLog)
+        {
+            if (textWriterTraceListener == null)
+            {
+                textWriterTraceListener = new TextWriterTraceListener(@"D:\Michael.log");
+                //System.Diagnostics.Trace.Listeners.Clear();
+                Trace.AutoFlush = true;
+                Trace.Listeners.Add(textWriterTraceListener);
+            }
+            Trace.WriteLine(DateTime.Now.ToString() + " " + strLog);
+        }
+    }
+}
