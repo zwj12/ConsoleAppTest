d7411973913f1cc8da42b0f91feacfe00d667cc8
Michael-WeiJin Zhu <michael-weijin.zhu@cn.abb.com>
2022-04-28 10:38:03 +0800

    update

---
+++ ConsoleAppTest/Log/SystemTraceSource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest.Log
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class SystemTraceSource
+    {
+        public SystemTraceSource()
+        {
+
+        }
+    }
+}
