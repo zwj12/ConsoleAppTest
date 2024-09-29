8b20734762ccc30082f996524b2d39503bf676b8
Michael-WeiJin Zhu <michael-weijin.zhu@cn.abb.com>
2024-09-29 13:25:17 +0800

    update

---
+++ ConsoleAppTest/MarshalInterop/MarshalHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ConsoleAppTest.MarshalInterop
+{
+    public static class MarshalHelper
+    {
+        public static T ByteArrayToStruct<T>(byte[] byteArray, bool checkSize = true) where T : struct
+        {
+            T obj = default;
+            int size = Marshal.SizeOf(obj);
+
+            if (checkSize == true && byteArray.Length != size)
+            {
+                throw new ArgumentException($"Byte array size ({byteArray.Length}) does not match the size ({size}) of the struct {typeof(T)}");
+            }
+
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            try
+            {
+                Marshal.Copy(byteArray, 0, ptr, size);
+                obj = (T)Marshal.PtrToStructure(ptr, typeof(T));
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(ptr);
+            }
+
+            return obj;
+        }
+
+        public static byte[] StructToByteArray<T>(T obj) where T : struct
+        {
+            int size = Marshal.SizeOf(obj);
+            byte[] byteArray = new byte[size];
+
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            try
+            {
+                Marshal.StructureToPtr(obj, ptr, true);
+                Marshal.Copy(ptr, byteArray, 0, size);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(ptr);
+            }
+
+            return byteArray;
+        }
+    }
+}
--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -1,25 +1,71 @@
 ﻿using System;
-using System.IO;
-using System.Net;
-using System.Runtime.Caching;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace ConsoleAppTest
 {
 
-    public class Program
+    public static class Program
     {
+        public enum ItemSourceMessageVersion
+        {
+            ITMSRC_MESSAGE_VERSION_1 = 1,
+            ITMSRC_MESSAGE_VERSION_2,
+            ITMSRC_MESSAGE_VERSION_3,
+            ITMSRC_MESSAGE_VERSION_4,
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 4)]
+        public struct ItmsrcItemStatisticsV2
+        {
+            public bool used;
+            public Int32 item_type;
+            public Int32 no_of_used;
+            public Int32 no_of_skipped;
+            public Int32 overflow;
+            public Int32 total_overflow;
+            public Int32 incoming;
+            public Int32 total_incoming;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 4)]
+        public struct ItmsrcContainerStatistics
+        {
+            public Int32 no_of_completed;
+            public Int32 no_of_incompleted;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 4)]
+        public struct ItmsrcStatisticsDataV2
+        {
+            public ItemSourceMessageVersion version;
+            public Int64 ct_time;
+            public Int32 itmtgt_number;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 20)]
+            public ItmsrcItemStatisticsV2[] item_stat;
+            public ItmsrcContainerStatistics container_stat;
+            public Int32 no_of_cnv_stops;
+        }
 
         public static void Main(string[] args)
         {
-            DirectoryInfo value = null;
-            value = Directory.CreateDirectory(@"");
+            ItmsrcStatisticsDataV2 itmsrcStatisticsData = new ItmsrcStatisticsDataV2();
+            ItmsrcContainerStatistics itmsrcContainerStatistics= new ItmsrcContainerStatistics();
+            ItmsrcItemStatisticsV2 itmsrcItemStatisticsV2 = new ItmsrcItemStatisticsV2();
+            int size1 = Marshal.SizeOf(itmsrcStatisticsData);
+            int size2 = Marshal.SizeOf(itmsrcItemStatisticsV2);
+            int size3 = Marshal.SizeOf(itmsrcContainerStatistics);
 
             Console.ReadKey();
         }
 
-
-
-
+     
 
     }
 
